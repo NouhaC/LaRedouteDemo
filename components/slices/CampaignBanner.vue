4ab7842b889ff66c1770ec15aa1db0f26d0d857e
Nouha Chhih <nouha.chhih@prismic.io>
2020-03-01 22:45:02 +0100

    make it better 5

--- components/slices/CampaignBanner.vue
+++ components/slices/CampaignBanner.vue
@@ -1,9 +1,8 @@
 <template>
-    <section class="full-width-image campaign-setion" :style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(' + slice.primary.image_banniere_de_campagne + ')' }">
-        <prismic-image :field="slice.primary.image_banniere_de_campagne "/>
-        <h2> {{slice.primary.campagne.title }} </h2>
+    <section class="campaign-section" :style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(' + slice.primary.image_banniere_de_campagne.url + ')' }">
+        <h2 class="campaign-title"> {{slice.primary.campagne.title }} </h2>
         <p class="campaign-description">{{ slice.primary.campagne.description }}</p>
-        <prismic-link :field="slice.primary.button_link"> {{ slice.primary.button_label }} </prismic-link>
+        <prismic-link class="campaign-button" :field="slice.primary.button_link"> {{ slice.primary.button_label }} </prismic-link>
     </section>
 </template>
 
@@ -15,7 +14,45 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.campaign-section
+  margin: -70px 0 80px
+  padding: 10em 0 8em
+  background-position: center center
+  background-size: cover
+  color: #ffffff
+  line-height: 1.75
+  text-align: center
+
+.campaign-section
+  text-align: center
+
+.campaign-title, .campaign-description
+  width: 90%
+  max-width: 490px
+  margin-left: auto
+  margin-right: auto
+
+.campaign-title
+  color: #ffffff
+  font-size: 70px
+  font-weight: 900
+  line-height: 70px
+
+.campaign-button
+  background: #ffffff
+  border-radius: 7px
+  color: #484D52
+  font-size: 14px
+  font-weight: 700
+  padding: 15px 40px
+  &:hover
+    background: #c8c9cb
+
 @media (max-width: 767px)
   .campaign-section
-    margin-bottom: 2rem
+    margin: 0 0 40px
+    padding: 10em 0 6em
+  .banner-title
+    font-size: 50px
+    line-height: 50px
 </style>
